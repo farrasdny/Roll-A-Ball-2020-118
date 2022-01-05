65fc5f1e15c881d2f64e79c762f3877af711d12b
Farras Danyka Putra <71580610+farrasdny@users.noreply.github.com>
2022-01-06 02:04:48 +0700

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+
+		SetCountText();
+		winTextObject.SetActive(false);
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count : " + count.ToString();
+
+		if(count >= 14)
+		{
+			winTextObject.SetActive(true);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+
+			SetCountText();
+		}
+	}
+}
